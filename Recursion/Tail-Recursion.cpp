f89663f1a3d753c8b60d8399148b7a93c3291152
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-02-25 20:53:15 +0530

    feat: tail recursion

---
+++ Recursion/Tail-Recursion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void func(int n) {
+    if (n > 0) {
+        cout << n << " ";
+        func(n - 1);
+    }
+}
+int main () {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    func(n);    
+    return 0;
+}
